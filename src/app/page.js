ed7810f6d75b3fe703ecc0bc27d3cb9f773a41b0
24LP3431 24LP3431 <24LP3431@G302-CC0115.local>
2024-12-17 18:25:32 +0800

    started new project form

--- src/app/page.js
+++ src/app/page.js
@@ -22,6 +22,7 @@ export default function Home() {
   return (
     <div className="flex justify-center mt-[200px]">
       <MainBody currentStep={currentStep} setCurrentStep={setCurrentStep} />
+      hi
     </div>
   );
 }
